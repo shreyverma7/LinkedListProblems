d6161043c6a185bf831b7961d87c5524e53934c1
Shrey Kumar Verma <shreykumar.v@tvsnext.com>
2023-07-27 10:04:25 +0530

    [Shrey] Add : Queue feture enqueue & Dequeue

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -5,16 +5,28 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("LinkedList Problem");
-        Console.WriteLine("Welcome to the Linked List Stack");
-        LinkedListStack stack = new LinkedListStack();
-        stack.Push(56);
-        stack.Push(30);
-        stack.Push(70);
-        stack.Display();
-        stack.Peek();
-        stack.Pop();
-        stack.Display();
+        Console.WriteLine("Queue Fuction");
+        LinKedListQueue queue = new LinKedListQueue(); 
+        queue.Enqueue(56);
+        queue.Enqueue(30);
+        queue.Enqueue(70);
+        queue.Display();
+        queue.Dequeue();
+        queue.Display();
+
+
+
+
+        //Console.WriteLine("LinkedList Problem");
+        //Console.WriteLine("Welcome to the Linked List Stack");
+        //LinkedListStack stack = new LinkedListStack();
+        //stack.Push(56);
+        //stack.Push(30);
+        //stack.Push(70);
+        //stack.Display();
+        //stack.Peek();
+        //stack.Pop();
+        //stack.Display();
 
         /* Console.WriteLine("LinkedList Problem");
          Console.WriteLine("Welcome to the Linked List Operation");
